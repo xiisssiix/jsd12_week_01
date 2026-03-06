4d331129c95089eb18740afc2423dfeb9609ab48
unknown <xiisssiix@gmail.com>
2026-03-06 14:06:58 +0700

    added 3 files

---
+++ array_and_object.js
@@ -0,0 +1,6 @@
+let learner = {name1:"Prem",age1:45,isgraduated:"RU",hobbies:["Basketball","Chess","Movies"]}
+
+console.log(learner.name1)
+console.log(learner.age1)
+console.log(learner.isgraduated)
+console.log(learner.hobbies[0])
